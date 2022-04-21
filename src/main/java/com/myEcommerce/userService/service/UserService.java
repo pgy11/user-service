d8baa6fc699c49455341af49deb60dd8490a414b
Giyoon Park <workspaceforict@gmail.com>
2022-04-22 01:46:34 +0900

    사용자 생성 기능 및 spring security 설정

--- src/main/java/com/myEcommerce/userService/service/UserService.java
+++ src/main/java/com/myEcommerce/userService/service/UserService.java
@@ -6,6 +6,7 @@ import com.myEcommerce.userService.jpa.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,15 +14,21 @@ import java.util.UUID;
 @Service
 public class UserService {
 
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
     @Autowired
-    UserRepository userRepository;
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+    }
 
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("encrypted_pwd");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
         userRepository.save(userEntity);
 
